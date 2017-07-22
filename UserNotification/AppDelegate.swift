b47db837d7efe5a039373d2dfa393dfb00df0b04
marcinoc <pietrzakmarcin@hotmail.com>
2017-07-22 14:08:17 +0200

    User Notification
    Fuun with userNotification

--- UserNotification/AppDelegate.swift
+++ UserNotification/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let notificationsCenter = UNUserNotificationCenter.current()
+        notificationsCenter.requestAuthorization(options: [.alert, .badge, .sound],  completionHandler: { (granted, error) in } )
         return true
     }
 
--- UserNotification/ViewController.swift
+++ UserNotification/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scheduleNotification()
+    }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Simple Notification"
+        content.subtitle = "Never ending learning"
+        content.body = "Simple Notification - Alert is Ready"
+        content.badge = 1
+        content.sound = UNNotificationSound.default()
+        content.userInfo = ["id":42]
+        
+        /*
+        //Add image to Notification
+        let imageURL = Bundle.main.url(forResource: "sample", withExtension: "jpg")
+        let attachment = UNNotificationAttachment(identifier: "sample.jpg", url: imageURL!, options: nil)
+         content.attachments = [attachment]
+        */
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0,
+                                                        repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "simple.notification",
+                                            content: content,
+                                            trigger: trigger)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        notificationCenter.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +54,17 @@ class ViewController: UIViewController {
 
 
 }
+
+    extension ViewController: UNUserNotificationCenterDelegate {
+        func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+            print(response.notification.request.content.userInfo)
+            completionHandler()
+        }
+        
+        func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            completionHandler([.alert, .sound])
+        }
+    }
+
+
 
